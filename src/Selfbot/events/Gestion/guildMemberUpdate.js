a6fcfd742d2404a91da23ccf8154ab6985be89a4
002-sans <002notsans@gmail.com>
2025-07-17 23:45:51 +0200

    a

--- src/Selfbot/events/Gestion/guildMemberUpdate.js
+++ src/Selfbot/events/Gestion/guildMemberUpdate.js
@@ -13,8 +13,8 @@ module.exports = {
         const oldRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
 
         for (const role of oldRoles.values()) {
-            const roleData = await role.fetch().catch(() => false);
-
+            const limitRole = client.db.limitrole.find(r => r.id === role.id || r.ID === role.id);
+            
             const max = limitRole.max ?? limitRole.MAX;
             const membersWithRole = newMember.guild.members.cache.filter(m => m.roles.cache.has(roleData.id)).size;
 
@@ -26,16 +26,15 @@ module.exports = {
         for (const role of addedRoles.values()) {
             const limitRole = client.db.limitrole.find(r => r.id === role.id || r.ID === role.id);
             if (limitRole) {
-                const roleData = await role.fetch().catch(() => false);
                 const max = limitRole.max ?? limitRole.MAX;
-                const membersWithRole = newMember.guild.members.cache.filter(m => m.roles.cache.has(roleData.id)).size;
+                const membersWithRole = newMember.guild.members.cache.filter(m => m.roles.cache.has(role.id)).size;
 
-                const newName = `${roleData.name.replace(/\s*\[\d+\/\d+\]$/, '')} [${membersWithRole}/${max}]`;
-                if (roleData.name !== newName)
-                    roleData.setName(newName);
+                const newName = `${role.name.replace(/\s*\[\d+\/\d+\]$/, '')} [${membersWithRole}/${max}]`;
+                if (role.name !== newName) 
+                    role.setName(newName);
 
                 if (membersWithRole > max) {
-                    await newMember.roles.remove(roleData.id, "Limite de membres atteinte pour ce rôle");
+                    await newMember.roles.remove(role.id, "Limite de membres atteinte pour ce rôle");
                 }
             }
         }
