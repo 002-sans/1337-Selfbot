1a35118f52b4c94eac87f5ec924353984f2a4ef6
002-sans <002notsans@gmail.com>
2025-07-17 23:44:32 +0200

    update

--- src/Selfbot/events/Gestion/guildMemberUpdate.js
+++ src/Selfbot/events/Gestion/guildMemberUpdate.js
@@ -10,16 +10,28 @@ module.exports = {
     */
     run: async (oldMember, newMember, client) => {
         const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+        const oldRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+
+        for (const role of oldRoles.values()) {
+            const roleData = await role.fetch().catch(() => false);
+
+            const max = limitRole.max ?? limitRole.MAX;
+            const membersWithRole = newMember.guild.members.cache.filter(m => m.roles.cache.has(roleData.id)).size;
+
+            const newName = `${roleData.name.replace(/\s*\[\d+\/\d+\]$/, '')} [${membersWithRole}/${max}]`;
+            if (roleData.name !== newName)
+                roleData.setName(newName);
+        }
 
         for (const role of addedRoles.values()) {
-            const limitRole = client.db.limitrole.find(r => r.id === roleData.id || r.ID === roleData.id);
+            const limitRole = client.db.limitrole.find(r => r.id === role.id || r.ID === role.id);
             if (limitRole) {
                 const roleData = await role.fetch().catch(() => false);
                 const max = limitRole.max ?? limitRole.MAX;
                 const membersWithRole = newMember.guild.members.cache.filter(m => m.roles.cache.has(roleData.id)).size;
 
                 const newName = `${roleData.name.replace(/\s*\[\d+\/\d+\]$/, '')} [${membersWithRole}/${max}]`;
-                if (roleData.name !== newName) 
+                if (roleData.name !== newName)
                     roleData.setName(newName);
 
                 if (membersWithRole > max) {
